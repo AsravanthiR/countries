db7d6f9aeecb515cc825fc2b3079a62e0a2e0a3e
AsravanthiR <88823383+AsravanthiR@users.noreply.github.com>
2021-11-23 12:40:42 +0200

    search input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return <div></div>;
-};
+import axios from "axios";
+import number from "easy-number-formatter";
+
+class App extends Component {
+  state = {
+    data: [],
+    searchIput: "",
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://restcountries.com/v2/all?fields=name,capital,flags,population,languages,currencies"
+      )
+      .then((res) => {
+        this.setState({ data: res.data });
+        console.log(this.state.data);
+      });
+  }
+  searchHandler = (e) => {
+    this.setState({
+      searchIput: e.target.value,
+    });
+    console.log(this.state.searchIput);
+  };
+  render() {
+    console.log(this.state.data);
+
+    return (
+      <div className="countries">
+        <input
+          type="text"
+          name="search"
+          onChange={this.searchHandler.bind(this)}
+        />
+
+        {this.state.data
+          .filter((country) => {
+            return country.name
+              .toLowerCase()
+              .includes(this.state.searchIput.toLowerCase());
+          })
+          .map((country) => (
+            <div className="country" key={country.name}>
+              <h2>{country.name} </h2>
+
+              <h3>{country.capital}</h3>
+              <div id="about">
+                <img src={country.flags.png} alt={country.name} />
+              </div>
+
+              <div className="cardContent">
+                <p>
+                  Languages:
+                  {country.languages.map((lang, i) => (
+                    <span key={i}> {lang.name} </span>
+                  ))}
+                </p>
+                <p>
+                  Currencies:
+                  {country.currencies.map((mon, i) => (
+                    <span key={i}> {mon.name} </span>
+                  ))}
+                </p>
+                <p> Population: {number.formatNumber(country.population)}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
 
 export default App;
