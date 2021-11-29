4c24f385499f361dd2a8f2e4ee05fd3ee1cd6d27
AsravanthiR <88823383+AsravanthiR@users.noreply.github.com>
2021-11-29 13:06:27 +0200

    added links

--- src/CountryCard.js
+++ src/CountryCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import number from "easy-number-formatter";
+import { Link } from "react-router-dom";
 
 const CountryCard = ({
   name,
@@ -10,30 +11,32 @@ const CountryCard = ({
   flags,
 }) => {
   return (
-    <div className="country" key={name}>
-      <h2>{name} </h2>
+    <Link to={capital}>
+      <div className="country" key={name}>
+        <h2>{name} </h2>
 
-      <h3>{capital}</h3>
-      <div id="about">
-        <img src={flags.png} alt={name} />
-      </div>
+        <h3>{capital}</h3>
+        <div id="about">
+          <img src={flags.png} alt={name} />
+        </div>
 
-      <div className="cardContent">
-        <p>
-          Languages:
-          {languages.map((lang, i) => (
-            <span key={i}> {lang.name} </span>
-          ))}
-        </p>
-        <p>
-          Currencies:
-          {currencies.map((mon, i) => (
-            <span key={i}> {mon.name} </span>
-          ))}
-        </p>
-        <p> Population: {number.formatNumber(population)}</p>
+        <div className="cardContent">
+          <p>
+            Languages:
+            {languages.map((lang, i) => (
+              <span key={i}> {lang.name} </span>
+            ))}
+          </p>
+          <p>
+            Currencies:
+            {currencies.map((mon, i) => (
+              <span key={i}> {mon.name} </span>
+            ))}
+          </p>
+          <p> Population: {number.formatNumber(population)}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <p>This is the home page</p>
+      <p>
+        <h1>Countries App</h1>
+      </p>
     </div>
   );
 };
